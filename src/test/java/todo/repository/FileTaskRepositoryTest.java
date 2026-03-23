946ee9675b44a6ba2d25384932a1de0f93bd5282
IvanRyskin <ryiskinivan@gmail.com>
2026-03-23 15:06:45 +0300

    Дописал тест к FileTaskRepository

--- src/test/java/todo/repository/FileTaskRepositoryTest.java
+++ src/test/java/todo/repository/FileTaskRepositoryTest.java
@@ -1,14 +1,19 @@
 package todo.repository;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import todo.model.Task;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentMap;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FileTaskRepositoryTest {
 
@@ -16,32 +21,55 @@ class FileTaskRepositoryTest {
     TaskRepository<Task> inMemoryRepository;
     Task task;
     Path path;
+    static List<Path> tempFiles = new ArrayList<>();
 
     @BeforeEach
     void init() {
         inMemoryRepository = new InMemoryTaskRepositoryMap();
         task = new Task(100, "Beer", "Drink", LocalDateTime.now());
-        path = Path.of("src/main/resources/tasks.txt");
+        path = Path.of("src/main/test_resources/tasks.txt");
+        fileRepository = new FileTaskRepository(path);
     }
 
+    @AfterAll
+    static void delete() {
+        tempFiles.forEach(path -> {
+            try {
+                Files.deleteIfExists(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
     @Test
     void createFile() {
-        fileRepository = new FileTaskRepository(path);
-        Path parent = Path.of("src/main/resources");
+        Path parent = Path.of("src/main/test_resources");
         Path fileName = Path.of("tasks.txt");
         assertEquals(parent, path.getParent());
         assertEquals(fileName, path.getFileName());
+        tempFiles.add(path);
     }
 
-    //    @Test
-//    void getData() {
-//    }
-//
+    @Test
+    void getDataFromEmptyFile() {
+        Path emtyPath = Path.of("src/main/test_resources/emptyTasks.txt");
+        FileRepository emptyFileRepository = new FileTaskRepository(emtyPath);
+        ConcurrentMap<Integer, Task> getFile = emptyFileRepository.getData();
+        assertTrue(getFile.isEmpty());
+        tempFiles.add(emtyPath);
+    }
+
+    @Test
+    void getDataFromNotEmtyFile() {
+        ConcurrentMap<Integer, Task> getFile = fileRepository.getData();
+        assertFalse(getFile.isEmpty());
+    }
+
     @Test
     void writeData() {
-        fileRepository = new FileTaskRepository(path);
         inMemoryRepository.addTask(task);
         assertTrue(fileRepository.writeData(inMemoryRepository.getTasksRepository()));
+        tempFiles.add(path);
     }
 }
